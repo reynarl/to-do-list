a0d11c7b022c92c1512e8da669da977413f088fc
Reeyna <reeynarl@gmail.com>
2022-07-12 14:56:21 -0500

    [ADD] logo with styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import logoFrecodecamp from './img/logo-freecodecamp.png'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={logoFrecodecamp} className='freecodecamp-logo' alt='Logo freecodecamp' />
+      </div>
     </div>
   );
 }

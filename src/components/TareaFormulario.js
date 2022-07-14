d0b4e6ab2b32bab6132a180ff9c7cb39a1c72579
Reeyna <reeynarl@gmail.com>
2022-07-14 16:08:33 -0500

    [ADD] task functions

--- src/components/TareaFormulario.js
+++ src/components/TareaFormulario.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { v4 as uuidv4 } from 'uuid'
+
 import '../styles/TareaFormulario.css'
 
-const TareaFormulario = () => {
+const TareaFormulario = (props) => {
+
+  const [input, setInput] = useState('')
+
+  const manejarCambio = e =>{
+    setInput(e.target.value)
+  }
+
+  const manejarEnvio = e =>{
+    e.preventDefault()
+    // Al enviar el formulario lo que hará será crear el objeto que rep´resentará la tarea nueva
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completada: false
+    }
+
+    props.onSubmit(tareaNueva)
+
+  }
+
   return (
-    <form className='tarea-formulario'>
+    <form onSubmit={manejarEnvio} className='tarea-formulario'>
       <input 
         type="text" 
         className="tarea-input"
         placeholder='Escribe una tarea'
         name='texto' 
+        onChange={manejarCambio}
       />
       <button className='tarea-boton'>Agregar</button>
     </form>
